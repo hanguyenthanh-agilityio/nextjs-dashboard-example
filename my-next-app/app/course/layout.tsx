8e9acd213bcb548767f5c6af07bd57ff37f7bfef
ha nguyen thanh <ha.nguyenthanh@asnet.com.vn>
2025-04-03 18:19:46 +0700

    Implement sidebar

--- my-next-app/app/course/layout.tsx
+++ my-next-app/app/course/layout.tsx
@@ -1,4 +1,4 @@
-import Sidebar from "./components/sidebar";
+import Sidebar from "../dashboard/components/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
